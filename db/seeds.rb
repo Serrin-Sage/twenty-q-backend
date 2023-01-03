478ef9f38f2e57f5e9780983394ad197d7bea593
colterlevi <cllongshore@gmail.com>
2023-01-03 14:49:13 -0500

    seeds that dont grow

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lobby = Lobby.create(lobbyname: "test")
+
+movie = Game.create(category: "Movie", lobby_id: 1)
+tv_show = Game.create(category: "TV Show", lobby_id: 1)
+video_game = Game.create(category: "Video Game", lobby_id: 1)
+book = Game.create(category: "Book", lobby_id: 1)
+person = Game.create(category: "Person", lobby_id: 1)
+object = Game.create(category: "Object", lobby_id: 1)
+place = Game.create(category: "Place", lobby_id: 1)
+food = Game.create(category: "Food", lobby_id: 1)
+animal = Game.create(category: "Animal", lobby_id: 1)
+misc = Game.create(category: "Misc.", lobby_id: 1)
