1d2e4b88fe4d5b638eaa3c25c82dce838d9661ef
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-04 09:30:00 -0500

    remvoed unnecessary tables

--- app/models/host.rb
+++
@@ -1,3 +0,0 @@
-class Host < ApplicationRecord
-    
-end
--- app/models/lobby.rb
+++
@@ -1,5 +0,0 @@
-class Lobby < ApplicationRecord
-
-    validates :name, presence: true
-    validates :player_number, numericality: true
-end
