ff4c70d453af756cbba536174a88c68dd499c2c5
colterlevi <cllongshore@gmail.com>
2023-01-05 09:21:31 -0500

    user creation and validation working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,18 @@ class UsersController < ApplicationController
     end
 
     def show
-        render json: User.find_by!(id: params[:id])
+        user = User.find_by!(id: params[:id])
+        render json: user
     end
     
     def create
-        user = User.create(name: params[:name], image: params[:image], lobby_id: params[:lobby_id])
-        render json: user
+        user = User.create(name: params[:name], image: params[:image])
+        if user.valid?
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: 422
+        end
     end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_and_belongs_to_many :lobby
+
+        validates :name, presence: true
+
 end
