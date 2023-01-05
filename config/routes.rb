6d40d1f6e5f4b3af6452e5de036da62786ff205b
Serrin <serrin.doscher53@myhunter.cuny.edu>
2023-01-05 16:27:43 -0500

    added operation to delete user

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
+  delete '/users/:id', to: 'users#destroy'
 
   get '/lobbies', to: 'lobbies#index'
   get '/lobbies/:id', to: 'lobbies#show'
