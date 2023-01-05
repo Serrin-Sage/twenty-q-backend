34f3a7664b0f3e2e967d967740d2451d9ce44c7e
Serrin <serrin.doscher53@myhunter.cuny.edu>
2023-01-05 11:00:26 -0500

    updated some models and controllers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,13 +16,3 @@
    end
  end
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
-
