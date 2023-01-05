deb2aa1ef718f2ec6aa8884f4f24f4af96d45f86
colterlevi <cllongshore@gmail.com>
2023-01-05 14:32:47 -0500

    indicidual chat rooms work but don't save messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
-    def index
-        message = Message.all
+
+    def index 
+        render json: Message.all
+    end
+    
+    def show
+        message = Message.where(lobby_id: params[:lobby_id])
         render json: message
     end
 
     def create
-        message = Message.create!(content: params[:content], user_id: 1)
-        ActionCable.server.broadcast('live_feed', {
+        message = Message.create(content: params[:content], user_id: params[:user_id], lobby_id: params[:lobby_id])
+        ActionCable.server.broadcast("chat_lobby_#{message.lobby_id}", {
             post: message
         })
         render json: message
